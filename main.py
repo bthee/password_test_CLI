4b24588fb80f49118bdd1b7898be99993b0f48db
bthee <b.theeck08@gmail.com>
2024-02-05 15:54:43 +0100

    improvements to all functions

--- main.py
+++ main.py
@@ -1,88 +1,65 @@
-import sys
-
 def start() -> int:
     user = input("Press 'enter' to 'begin' or type anything to 'exit'.")
-    if user != "":
+    if user:
         print("Goodbye...")
-        sys.exit()
+        return 1
     return 0
 
-
-def password_inpout() -> str:
-    pw = input("Type the password for testing: ")
-    return pw
-
+def password_input() -> str:
+    while True:
+        try:
+            pw = input("Type the password for testing: ")
+            return pw
+        except:
+            print("Please enter a valid password.")
 
 def password_check(pw: str) -> tuple:
-    length = False
-    number = False
-    upper_ch = False
-    lower_ch = False
-    special_ch = False
-    count = 0
+    length = len(pw) >= 12
+    number = any(char.isnumeric() for char in pw)
+    upper_ch = any(char.isupper() for char in pw)
+    lower_ch = any(char.islower() for char in pw)
+    special_ch = any(not char.isalnum() for char in pw)
 
-    if len(pw) >= 12:
-        length = True
-    
-    for i in pw:
-        if i.isnumeric():
-            number = True
-        if i.isupper():
-            upper_ch = True
-        if i.islower():
-            lower_ch = True
-        if not i.isnumeric() and not i.isalpha():
-            special_ch = True
-
-    if length:
-        count += 1
-    if number:
-        count += 1
-    if upper_ch:
-        count += 1
-    if lower_ch:
-        count += 1
-    if special_ch:
-        count += 1
-        
-    return (length, number, upper_ch, lower_ch, special_ch, count)
+    count = sum([length, number, upper_ch, lower_ch, special_ch])
+    return length, number, upper_ch, lower_ch, special_ch, count
 
 
-def evaluation(checked_pw: tuple) -> int:
+def evaluation(checked_pw: tuple) -> None:
     length, number, upper_ch, lower_ch, special_ch, count = checked_pw
 
+    print("Password Strength Evaluation:")
     if count == 5:
         print("Your password is strong!")
     elif count == 4 and length:
         print("Your password is good!\nHere is an improvement you should make.")
     elif count == 4 and not length:
-        print("Your password would be strong, if you ...")
+        print("Your password would be strong if you increase its length.")
     elif count == 3 and length:
         print("Your password is okay. Here are some improvements you should make.")
     elif count == 3 and not length:
-        print("Your password would be strong, if you ...")
+        print("Your password would be strong if you increase its length.")
     elif count <= 2:
         print("\nYour password is very weak! You should make the following improvements!")
 
-    if not length:
-        print("- Increase the length of your password to atleast 12 characters.")
+    if not length and count <= 2:
+        print("- Increase the length of your password to at least 12 characters.")
     if not number:
-        print("- Include atleast one number character like '1, 2, 3 ...'")
+        print("- Include at least one number character like '1, 2, 3 ...'")
     if not upper_ch:
-        print("- Include atleast one uppercase character like 'A, B, C ...'")
+        print("- Include at least one uppercase character like 'A, B, C ...'")
     if not lower_ch:
-        print("- Include atleast one lowercase character like 'a, b, c ...'")
+        print("- Include at least one lowercase character like 'a, b, c ...'")
     if not special_ch:
-        print("- Include atleast one special character like '@, $, % ...'")
-
+        print("- Include at least one special character like '@, $, % ...'")
 
 def main() -> int:
     print("Welcome! Here you can test the strength of your password.\n")
-    start()
-    pw = password_inpout()
+    if start():
+        return 0
+    
+    pw = password_input()
     checked_pw = password_check(pw)
-    eval = evaluation(checked_pw)
-
+    evaluation(checked_pw)
 
 if __name__ == "__main__":
     main()
